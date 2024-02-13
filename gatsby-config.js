7c4ea3b57d5592fee7e37ccd7ef033e77e947524
chelstang <86565244+chelstang@users.noreply.github.com>
2024-02-12 20:20:53 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Testing for p5 integration`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
